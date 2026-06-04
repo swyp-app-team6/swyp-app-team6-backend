533a1367ee809289e30707f6c942405ac333387a
wodnd0131 <wddy2001@gmail.com>
2026-06-05 04:08:36 +0900

    build: init project

---
+++ src/main/java/org/swyp/com/backend/SwypAppBackendApplication.java
@@ -0,0 +1,12 @@
+package org.swyp.com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwypAppBackendApplication {
+
+     static void main(String[] args) {
+        SpringApplication.run(SwypAppBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/org/swyp/com/backend/SwypAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swyp.com.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwypAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
