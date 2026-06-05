78dc3494ff70ce06d25a81035a94f377822aa8c1
warmcotton <wonseok0807@naver.com>
2026-06-06 00:32:59 +0900

    feat: LoginRequest Validation 적용

--- src/main/java/org/swyp/com/backend/login/controller/LoginController.java
+++ src/main/java/org/swyp/com/backend/login/controller/LoginController.java
@@ -2,6 +2,7 @@ package org.swyp.com.backend.login.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.swyp.com.backend.login.controller.api.LoginApiSpec;
 import org.swyp.com.backend.login.dto.LoginRequest;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -18,7 +20,7 @@ public class LoginController implements LoginApiSpec {
     @Override
     @PostMapping("/login")
     public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request) {
-        System.out.println(request.id());
+        log.info(request.email(), request.password());
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/swyp/com/backend/login/dto/LoginRequest.java
+++ src/main/java/org/swyp/com/backend/login/dto/LoginRequest.java
@@ -1,7 +1,22 @@
 package org.swyp.com.backend.login.dto;
 
-import software.amazon.awssdk.annotations.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-public record LoginRequest(@NotNull String id) {
+public record LoginRequest(
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
+        String email,
+
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Size(min = 8, max = 14, message = "비밀번호는 8자 이상 14자 이하로 입력해주세요.")
+        @Pattern(
+                regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[*_!@#$%^&+=]).+$",
+                message = "비밀번호는 최소 하나의 대문자, 숫자, 특수문자(*_!@#$%^&+=)를 포함해야 합니다."
+        )
+        String password
+) {
 
 }
