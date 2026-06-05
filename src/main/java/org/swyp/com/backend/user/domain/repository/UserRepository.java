6ae9d4eeb759b874c41ab6ab92e342dadfce3c70
warmcotton <wonseok0807@naver.com>
2026-06-06 01:45:02 +0900

    feat: UserRepository 인터페이스 추가
    - JPA기반 리포지토리 추상화하기 위해 UserRepository 인터페이스 추가
    - UserRepository 구현한 JpaUserRepository 클래스 추가

---
+++ src/main/java/org/swyp/com/backend/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.swyp.com.backend.user.domain.repository;
+
+import java.util.Optional;
+import org.swyp.com.backend.user.domain.User;
+
+public interface UserRepository {
+    Optional<User> findByEmail(String email);
+    User save(User user);
+}
