a953634727eeef75469945512bc1cf179b2f8332
warmcotton <wonseok0807@naver.com>
2026-06-06 00:35:57 +0900

    feat: User 엔티티, UserRepository 추가

---
+++ src/main/java/org/swyp/com/backend/user/domain/User.java
@@ -0,0 +1,17 @@
+package org.swyp.com.backend.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+}
