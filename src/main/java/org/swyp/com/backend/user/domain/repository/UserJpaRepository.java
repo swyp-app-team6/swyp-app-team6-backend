52fac7b61827a215b56768a29e1b78b138fc532d
wodnd0131 <wddy2001@gmail.com>
2026-06-15 17:03:03 +0900

    refactor: SSO 추가에 따른 User 필드 값 추가

--- src/main/java/org/swyp/com/backend/user/domain/repository/UserJpaRepository.java
+++ src/main/java/org/swyp/com/backend/user/domain/repository/UserJpaRepository.java
@@ -2,8 +2,11 @@ package org.swyp.com.backend.user.domain.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.swyp.com.backend.global.enumeration.OAuthProvider;
 import org.swyp.com.backend.user.domain.User;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByProviderAndProviderUserId(OAuthProvider provider, String providerUserId);
 }
--- src/main/java/org/swyp/com/backend/user/domain/repository/UserRepositoryImpl.java
+++ src/main/java/org/swyp/com/backend/user/domain/repository/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.swyp.com.backend.user.domain.repository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.swyp.com.backend.global.enumeration.OAuthProvider;
 import org.swyp.com.backend.user.domain.User;
 
 @Repository
@@ -13,6 +14,11 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public Optional<User> findByEmail(String email) {
         return userJpaRepository.findByEmail(email);
+    }
+
+    @Override
+    public Optional<User> findByProviderAndProviderUserId(OAuthProvider provider, String providerUserId) {
+        return userJpaRepository.findByProviderAndProviderUserId(provider, providerUserId);
     }
 
     @Override
