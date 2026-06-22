801d067a9cff965a25ea823de3dd4b2508e6e4d9
warmcotton <wonseok0807@naver.com>
2026-06-22 15:58:36 +0900

    feat: Profile 엔티티 User 외래키 추가

--- src/main/java/org/swyp/com/backend/profile/domain/Profile.java
+++ src/main/java/org/swyp/com/backend/profile/domain/Profile.java
@@ -4,18 +4,25 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.util.Date;
 import java.util.UUID;
 import org.swyp.com.backend.global.enumeration.Gender;
+import org.swyp.com.backend.user.domain.User;
 
 @Entity
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
     @Column(nullable = false, length = 10)
     private String nickname;
     @Enumerated(EnumType.STRING)
