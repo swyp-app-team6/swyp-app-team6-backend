50c08d482cda4e903207eea4d6a02f7dff9b6e37
wodnd0131 <wddy2001@gmail.com>
2026-06-30 17:33:23 +0900

    fix: ProfileInterest에 @JoinColumn 누락 추가

--- src/main/java/org/swyp/com/backend/profile/domain/ProfileInterest.java
+++ src/main/java/org/swyp/com/backend/profile/domain/ProfileInterest.java
@@ -5,6 +5,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
@@ -14,8 +15,10 @@ public class ProfileInterest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JoinColumn(name = "profile_id", nullable = false)
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private Profile profile;
+    @JoinColumn(name = "interest_id", nullable = false)
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private Interest interest;
 
