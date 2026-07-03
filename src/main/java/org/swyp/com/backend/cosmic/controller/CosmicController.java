754b54d442e807c3f9d71276655d46c120f36d76
warmcotton <wonseok0807@naver.com>
2026-07-03 09:19:29 +0900

    feat: 단일 Cosmic 조회 API 추가

--- src/main/java/org/swyp/com/backend/cosmic/controller/CosmicController.java
+++ src/main/java/org/swyp/com/backend/cosmic/controller/CosmicController.java
@@ -3,11 +3,14 @@ package org.swyp.com.backend.cosmic.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.swyp.com.backend.cosmic.controller.api.CosmicControllerApiSpec;
 import org.swyp.com.backend.cosmic.dto.CosmicTestResponse;
+import org.swyp.com.backend.cosmic.dto.CosmicTypeResponse;
 import org.swyp.com.backend.cosmic.service.CosmicService;
+import org.swyp.com.backend.global.enumeration.CosmicDatingType;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +22,11 @@ public class CosmicController implements CosmicControllerApiSpec {
     public ResponseEntity<CosmicTestResponse> getCosmicTestResponse() {
         CosmicTestResponse cosmicTestResponse = cosmicService.getCosmicTestResponse();
         return ResponseEntity.ok(cosmicTestResponse);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<CosmicTypeResponse> getCosmicType(@PathVariable("type") CosmicDatingType type) {
+        CosmicTypeResponse response = cosmicService.getCosmicTypeResponse(type);
+        return ResponseEntity.ok(response);
     }
 }
