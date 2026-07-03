d8a56514ff0fec2c0afc3990e56749594420418c
warmcotton <wonseok0807@naver.com>
2026-07-03 09:02:21 +0900

    feat: Cosmic 양방향 매핑 추가

--- src/main/java/org/swyp/com/backend/cosmic/domain/Cosmic.java
+++ src/main/java/org/swyp/com/backend/cosmic/domain/Cosmic.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import org.swyp.com.backend.global.enumeration.CosmicDatingType;
 
@@ -25,4 +29,13 @@ public class Cosmic {
     private String imageKey;
     @Column(nullable = false)
     private Boolean deleted = false;
+
+    @OneToMany(mappedBy = "cosmic", fetch = FetchType.LAZY)
+    private List<CosmicFeature> featureList = new ArrayList<>();
+    @OneToMany(mappedBy = "cosmic", fetch = FetchType.LAZY)
+    private List<CosmicMatch> matchList = new ArrayList<>();
+    @OneToMany(mappedBy = "cosmic", fetch = FetchType.LAZY)
+    private List<CosmicMention> mentionList = new ArrayList<>();
+    @OneToMany(mappedBy = "cosmic", fetch = FetchType.LAZY)
+    private List<CosmicTag> tagList = new ArrayList<>();
 }
